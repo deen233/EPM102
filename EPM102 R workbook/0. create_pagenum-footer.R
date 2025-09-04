d461235a7c6591316b8fb7db8dc8f5dec19bef1b
ShamsudeenMax <shamsudeen.mohammed1@lshtm.ac.uk>
2025-09-04 02:41:37 +0100

    update_sm

---
+++ EPM102 R workbook/0. create_pagenum-footer.R
@@ -0,0 +1,12 @@
+
+################################################################################
+# Create pagenum-footer.tex for r markdown page numbering
+###############################################################################
+
+\usepackage{fancyhdr}
+\pagestyle{fancy}
+\fancyhf{}
+\fancyfoot[C]{\thepage}
+\renewcommand{\headrulewidth}{0pt}
+\renewcommand{\footrulewidth}{0pt}
+
